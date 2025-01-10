fc7d5811278dc513ad19c8de3a51af08d1d72274
zion <atibjawad.zion@optimizely.com>
2025-01-11 04:42:15 +0600

    fix layout shift

--- src/app/configure/page.tsx
+++ src/app/configure/page.tsx
@@ -9,10 +9,15 @@ import ConfigureForm from "@/components/configure-form";
 export default async function Configure() {
   const units = await getUnits();
   return (
-    <main className="h-screen w-screen flex flex-col gap-5 items-center justify-center">
-      <h2 className="font-bold text-xl text-center ">Configure your units</h2>
-      <div className="flex flex-col gap-5 w-4/5 md:w-1/3">
+    <main className="h-screen w-screen flex flex-col gap-5 items-center">
+      <div className="basis-1/3 flex flex-col justify-end">
+        <h2 className="font-bold text-xl text-center ">Configure your units</h2>
+      </div>
+      <div className="w-4/5 md:w-1/3">
         <ConfigureForm />
+      </div>
+
+      <div className="flex flex-col gap-5 w-4/5 md:w-1/3">
         <Suspense fallback={<UnitSuspense />}>
           <UnitBox prefetchedUnits={units} />
         </Suspense>
