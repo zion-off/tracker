8b0283c9ff6f02083f15e3c42d37cb7c26b0571d
zion <atibjawad.zion@optimizely.com>
2025-01-12 07:58:57 +0600

    fix layout shift

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -5,12 +5,15 @@ import { getChartData } from "@/actions/get-chart";
 export default async function Chart() {
   const chart = await getChartData(new Date().getFullYear().toString());
   return (
-    <div className="w-5/6 md:max-w-[900px] my-4 border border-slate-200 dark:border-zinc-800 p-5 rounded-md overflow-x-scroll">
+    <div className="w-5/6 md:max-w-[900px] basis-1/5 h-auto items-end flex">
+      <div className="my-4 border border-slate-200 dark:border-zinc-800 p-5 rounded-md overflow-x-scroll ">
       <TableLayout>
         <div className="h-full w-full grid grid-rows-7 grid-flow-col">
           <Dots chart={chart} />
         </div>
       </TableLayout>
+      </div>
+      
     </div>
   );
 }
--- src/components/logger.tsx
+++ src/components/logger.tsx
@@ -4,10 +4,12 @@ import { getUnitsCount } from "@/actions";
 export default async function Logger() {
   const units = await getUnitsCount();
   return (
-    <div className="flex flex-wrap gap-3 my-4 w-5/6 md:max-w-[900px] max-h-28">
-      {units.map((item, index) => (
-        <Contribution key={index} unit={item} />
-      ))}
+    <div className="w-5/6 md:max-w-[900px] overflow-y-scroll basis-1/4 fade-out hide-scrollbar">
+      <div className="flex flex-wrap gap-3 my-4 ">
+        {units.map((item, index) => (
+          <Contribution key={index} unit={item} />
+        ))}
+      </div>
     </div>
   );
 }
