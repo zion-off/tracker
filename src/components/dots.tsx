a0585a9853de506463265990037bb6633de3a770
zion <atibjawad.zion@optimizely.com>
2025-01-13 08:50:55 +0600

    removed cookie effect in dots

--- src/components/dots.tsx
+++ src/components/dots.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import Link from "next/link";
 import { useEffect } from "react";
 
 import Dot from "./dot";
-import { useToast } from "@/hooks/use-toast";
-import { ToastAction } from "@/components/ui/toast";
 import { useHomeContext } from "@/context";
 import { ChartWithColorsType } from "@/interfaces";
 
 export default function Dots({ chart }: { chart: ChartWithColorsType[] }) {
-  const { toast } = useToast();
   const { dots, setAllDots, updateMaxValue } = useHomeContext();
 
   const maxValue = chart.reduce(
@@ -21,24 +17,6 @@ export default function Dots({ chart }: { chart: ChartWithColorsType[] }) {
   useEffect(() => {
     updateMaxValue(maxValue);
     setAllDots(chart);
-  }, []);
-
-  useEffect(() => {
-    const cookieValue = false;
-    if (cookieValue) {
-      setTimeout(() => {
-        toast({
-          title: `Hi ${cookieValue}, new here?`,
-          description:
-            "Set up your units and then come back here to log your daily contributions!",
-          action: (
-            <Link href={"/configure"}>
-              <ToastAction altText="Redirect">Configure</ToastAction>
-            </Link>
-          ),
-        });
-      }, 2000);
-    }
   }, []);
 
   return (
