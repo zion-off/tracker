080d2416082ab9cbe3295c5afe4f18b4ff51933c
zion <atibjawad.zion@optimizely.com>
2025-01-13 17:18:17 +0600

    add transitions to hover box

--- src/components/ui/hover-box.tsx
+++ src/components/ui/hover-box.tsx
@@ -14,7 +14,7 @@ export default function HoverBox({
   return (
     <div
       className={cn(
-        "absolute pointer-events-none text-neutral-800 dark:text-gray-200 rounded-md border bg-gray-200 dark:bg-neutral-800 dark:border-neutral-700 p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 text-xxs",
+        "absolute pointer-events-none text-neutral-800 dark:text-gray-200 rounded-md border bg-gray-200 dark:bg-neutral-800 dark:border-neutral-700 p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 text-xxs z-50",
         className
       )}
       style={{
