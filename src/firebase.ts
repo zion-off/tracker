8fe73186388e3d7f3bd7349f70a7b85c302863d3
zion <atibjawad.zion@optimizely.com>
2025-01-14 07:45:25 +0600

    update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,25 +1,13 @@
 import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-function getFirebasePrivateKey(): string {
-  const privateKey = process.env.FIREBASE_PRIVATE_KEY;
-
-  if (!privateKey) {
-    throw new Error(
-      "FIREBASE_PRIVATE_KEY is not set in the environment variables"
-    );
-  }
-
-  return privateKey.replace(/\\n/g, "\n");
-}
-
 if (!getApps().length) {
   try {
     initializeApp({
       credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: getFirebasePrivateKey(),
+        privateKey: process.env.FIREBASE_PRIVATE_KEY,
       }),
     });
   } catch (error) {
