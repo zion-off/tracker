820528d01d5f9ad22d76fd1b8120140a2ce99a1e
zion <atibjawad.zion@optimizely.com>
2025-01-14 07:42:24 +0600

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const result = require("dotenv").config();
+
+const nextConfig = {
+  reactStrictMode: true,
+  env: result.parsed,
+};
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
