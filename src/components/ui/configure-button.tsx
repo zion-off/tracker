0cac5d4ba775a06905fd152f1bfb011ec20fa618
zion <atibjawad.zion@optimizely.com>
2025-01-14 22:24:19 +0600

    fix seo

--- src/components/ui/configure-button.tsx
+++ src/components/ui/configure-button.tsx
@@ -3,7 +3,7 @@ import { Settings } from "lucide-react";
 
 export default function ConfigureLink() {
   return (
-    <Link href="/configure" className="absolute bottom-5 left-5">
+    <Link aria-description="Configure"  href="/configure" className="absolute bottom-5 left-5">
       <Settings
         size={25}
         className="p-1 bg-gray-100 stroke-gray-500 border-gray-50 hover:dark:bg-zinc-800 dark:bg-zinc-900 dark:stroke-zinc-300 rounded-full dark:border dark:border-zinc-800 cursor-pointer hover:animate-wiggle hover:dark:stroke-zinc-300 hover:stroke-gray-500"
