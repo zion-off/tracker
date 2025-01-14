ee84207cfeae0c6465d05496eed06eba0f071803
zion <atibjawad.zion@optimizely.com>
2025-01-14 08:38:31 +0600

    update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -9,7 +9,7 @@ if (!getApps().length) {
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey:
           process.env.NODE_ENV === "development"
-            ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string)
+            ? process.env.FIREBASE_PRIVATE_KEY as string
             : process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       }),
     });
