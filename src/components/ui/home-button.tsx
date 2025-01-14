b2996a02fee9dc0aaf2e1ef2748bf369701c7e59
zion <atibjawad.zion@optimizely.com>
2025-01-14 22:31:25 +0600

    fix aria labels

--- src/components/ui/home-button.tsx
+++ src/components/ui/home-button.tsx
@@ -3,7 +3,7 @@ import { House } from "lucide-react";
 
 export default function HomeLink() {
   return (
-    <Link aria-description="Home" href="/" className="absolute bottom-5 left-5">
+    <Link aria-label="Home" aria-description="Log your daily contributions" href="/" className="absolute bottom-5 left-5">
       <House
         size={25}
         className="p-1 bg-gray-100 stroke-gray-500 border-gray-50 hover:dark:bg-zinc-800 dark:bg-zinc-900 dark:stroke-zinc-300 rounded-full dark:border dark:border-zinc-800 cursor-pointer"
